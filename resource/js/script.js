3bf8425b503ab4c50af0bdfe39fff3ff4653e5b1
darneees <dgs56301@gmail.com>
2024-02-04 10:17:20 -0300

    create loader and button to top

---
+++ resource/js/script.js
@@ -0,0 +1,23 @@
+// button to scroll to top of page
+
+let myButton = document.getElementById("scrollTop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// loader for page
+
+setTimeout(function(){
+    $('#loader').fadeToggle();
+}, 2200);
